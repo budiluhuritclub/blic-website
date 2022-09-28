894e78d54e7256293cc2bb47151b82104c2f5fd6
Rylanristia <50695667+rylanristia@users.noreply.github.com>
2022-09-28 14:06:40 +0700

    layout fixing

--- resources/views/pages/admin/devcamp/create.blade.php
+++ resources/views/pages/admin/devcamp/create.blade.php
@@ -5,20 +5,22 @@
 @endsection
 
 @section('content')
-    <div class="card p-5">
-        <h4>Add new batch for Devcamp - Intensive Bootcamp</h4>
-        <form action="{{ route('batch.store') }}" method="POST">
-            @csrf
-            <div class="form-group mt-3">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Batch 01" disabled>
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="card p-5">
+            <h4>Add new batch for Devcamp - Intensive Bootcamp</h4>
+            <form action="{{ route('batch.store') }}" method="POST">
+                @csrf
+                <div class="form-group mt-3">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Batch 01" disabled>
+                    </div>
+                    <div class="input-group mt-3">
+                        <input type="text" class="form-control" placeholder="Masukan tema devcamp untuk batch terbaru"
+                            aria-label="title" id="title" name="title">
+                    </div>
+                    <button class="btn btn-primary mt-3" type="submit">Save</button>
                 </div>
-                <div class="input-group mt-3">
-                    <input type="text" class="form-control" placeholder="Masukan tema devcamp untuk batch terbaru"
-                        aria-label="title" id="title" name="title">
-                </div>
-                <button class="btn btn-primary mt-3" type="submit">Save</button>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 @endsection
